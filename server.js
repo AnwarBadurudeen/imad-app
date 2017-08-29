c4219036f5cc584eb2981fbccce2661a6c32f424
AnwarBadurudeen <30874298+AnwarBadurudeen@users.noreply.github.com>
2017-08-28 22:01:44 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,10 +59,7 @@ function createTemplate(data){
         </head>
     <body>
     <div class="container">
-        <div><a href="/">Home</a>&nbsp;&nbsp;&nbsp;
-        <a href="/article-one">article-one</a>&nbsp;&nbsp;&nbsp;
-        <a href="/article-two">article-two</a>&nbsp;&nbsp;&nbsp;
-        <a href="/article-three">article-three</a>&nbsp;&nbsp;&nbsp;</div>
+       
         
         <hr/>
         
