b880d4cf27f2fa9431370368ae1bd922ee794698
AnwarBadurudeen <30874298+AnwarBadurudeen@users.noreply.github.com>
2017-08-28 21:24:11 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,8 +29,18 @@ var articles={
             <p>This is my written webpage to the hasura website This is my written webpage to the hasura website
             This is my written webpage to the hasura website</p>`
 
+},   
+  'article-three':{
+    title:'Article one page',
+    heading:'Article one',
+    date:'sep 5,2017',
+    content:` <p>This is my written webpage to the hasura website This is my written webpage to the hasura website
+            This is my written webpage to the hasura website</p>
+            <p>This is my written webpage to the hasura website This is my written webpage to the hasura website
+            This is my written webpage to the hasura website</p>
+            <p>This is my written webpage to the hasura website This is my written webpage to the hasura website
+            This is my written webpage to the hasura website</p>`
 }
-    
 };
 
 
