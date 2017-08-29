5a9942b830779216d86d9f9c2b1587fbe04458a7
AnwarBadurudeen <30874298+AnwarBadurudeen@users.noreply.github.com>
2017-08-29 09:33:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ console.log('Loaded!');
  //element.innerHTML="I changed my content to test js";
  
  //to move image on click
- var img=document.getElementById('ma');
- var marginLeft=0;
- function moveRight(){
-     marginLeft=marginLeft + 1;
-     img.style.marginLeft=marginLeft + 'px';
- }
+ //var img=document.getElementById('ma');
+ //var marginLeft=0;
+ //function moveRight(){
+     //marginLeft=marginLeft + 1;
+    // img.style.marginLeft=marginLeft + 'px';
+// }
  img.ondblclick=function() {
  var interval=setInterval(moveRight,50); 
  };//for image give the id command in img tag not in div
