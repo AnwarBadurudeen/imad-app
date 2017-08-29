6b4a424a8127c58b146d831fdd802dae0a144eaa
AnwarBadurudeen <30874298+AnwarBadurudeen@users.noreply.github.com>
2017-08-28 23:42:51 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ console.log('Loaded!');
  element.innerHTML="I changed my content to test js";
  
  //to move image on click
- var img=document.getElementById("mad");
+ var img=document.getElementById('mad');
  img.onclick=function() {
   img.style.marginleft='100px';   
  };
